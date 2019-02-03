817e28899e64587463bbeaf4c98866a6eea2973a
Sean Kelly <shonkelli@gmail.com>
2019-02-03 17:22:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -12,7 +12,7 @@ end
 def countdown_with_sleep(input)
   # binding.pry
   while input > 0 do
-    puts "-#{input} SECOND(S)!\n"
+    puts "#{input} SECOND(S)!\n"
     input -= 1
     sleep(1)
   end
