10a3d671f394b279dfcd0df11d280232ede5ed90
Sean Kelly <shonkelli@gmail.com>
2019-02-03 17:20:31 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,3 +9,13 @@ def countdown(input)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(input)
+  # binding.pry
+  while input > 0 do
+    puts "-#{input} SECOND(S)!"
+    input -= 1
+    sleep(0.5)
+  end
+  "HAPPY NEW YEAR!"
+end
